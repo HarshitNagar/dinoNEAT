17b60a4909db9e2fc7c8dbb056bc640f28d469e8
Harshit Nagar <hnagar.000@gmail.com>
2018-10-16 04:47:25 +0530

    clean code

--- dino.py
+++ dino.py
@@ -1,53 +1,42 @@
-
-
-
+#NEUROEVOLUTION OF AUGMENTED TOPOLOGIES on DINO RUN
 import os
 import pickle
 #import visualize
 import neat
-
-runs_per_neat = 5
-simulation_seconds = 60.0
-
 import os
 import sys
 import pygame
 import random
 from pygame import *
 from time import sleep
+from math import exp
 
 pygame.init()
 
 scr_size = (width,height) = (600,150)
-FPS = 200
+FPS = 60
 gravity = 0.6
 
+runs_per_net = 5
 
-################################################################################
 dino_left = 40 #const
 dino_right = 84 #const
 dino_top = 0 #mapped
 dino_bottom = 0 #mapped
-################################################################################
 bird_left = 0 #mapped
 bird_right = 0 #mapped
 bird_top = 70 #const
 bird_bottom = 110 #const
-################################################################################
-cact_left = 0 #
-cact_right = 0 #
+cact_left = 0 #mapped
+cact_right = 0 #mapped
 cact_top = 107 #const
 cact_bottom = 147 #const
-################################################################################
-#ONLY ONE CACTUS AT A TIME SO THIS IS ONLY FOR FUTURE ENHANCEMENT
 cact2_left = 0 #not mapped
 cact2_right = 0 #not mapped
 cact2_top = 0 #const
 cact2_bottom = 0 #const
-################################################################################
 game_speed = 0 #mapped
 cur_score = 0 #mapped
-################################################################################
 
 
 black = (0,0,0)
@@ -349,53 +338,6 @@ class Scoreboard():
             self.temprect.left += self.temprect.width
         self.temprect.left = 0
 
-
-def introscreen():
-    temp_dino = Dino(44,47)
-    temp_dino.isBlinking = True
-    gameStart = False
-
-    #callout,callout_rect = load_image('call_out.png',196,45,-1)
-    #callout_rect.left = width*0.05
-    #callout_rect.top = height*0.4
-
-    temp_ground,temp_ground_rect = load_sprite_sheet('ground.png',15,1,-1,-1,-1)
-    temp_ground_rect.left = width/20
-    temp_ground_rect.bottom = height
-
-    logo,logo_rect = load_image('logo.png',240,40,-1)
-    logo_rect.centerx = width*0.6
-    logo_rect.centery = height*0.6
-    while not gameStart:
-        if pygame.display.get_surface() == None:
-            print("Couldn't load display surface")
-            return True
-        else:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w or event.key == pygame.K_UP:
-                        temp_dino.isJumping = True
-                        temp_dino.isBlinking = False
-                        temp_dino.movement[1] = -1*temp_dino.jumpSpeed
-
-        temp_dino.update()
-
-        if pygame.display.get_surface() != None:
-            screen.fill(background_col)
-            screen.blit(temp_ground[0],temp_ground_rect)
-            if temp_dino.isBlinking:
-                screen.blit(logo,logo_rect)
-                screen.blit(callout,callout_rect)
-            temp_dino.draw()
-
-            pygame.display.update()
-
-        clock.tick(FPS)
-        if temp_dino.isJumping == False and temp_dino.isBlinking == False:
-            gameStart = True
-
 def simulate_duck(playerDino):
     if not (playerDino.isJumping and playerDino.isDead):
         playerDino.isDucking = True
@@ -415,7 +357,10 @@ def simulate_unduck(playerDino):
     return
 
 
-def gameplay(net, fitnesses):
+def gameplay(genome, config):
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    fitnesses = []
+
     global high_score
     global bird_right
     global bird_left
@@ -431,112 +376,87 @@ def gameplay(net, fitnesses):
     X = 0 #jump when 1 not jump when 0
     Y = 0 #duck when 1 not duck when 0
 
+    for runs in range(runs_per_net):
+        fitness = 0.0
+        gamespeed = 4
+        startMenu = False
+        gameOver = False
+        gameQuit = False
+        playerDino = Dino(44,47)
+        new_ground = Ground(-1*gamespeed)
+        scb = Scoreboard()
+        highsc = Scoreboard(width*0.78)
+        counter = 0
 
-    gamespeed = 4
-    startMenu = False
-    gameOver = False
-    gameQuit = False
-    playerDino = Dino(44,47)
-    new_ground = Ground(-1*gamespeed)
-    scb = Scoreboard()
-    highsc = Scoreboard(width*0.78)
-    counter = 0
+        cacti = pygame.sprite.Group()
+        pteras = pygame.sprite.Group()
+        clouds = pygame.sprite.Group()
+        last_obstacle = pygame.sprite.Group()
 
-    cacti = pygame.sprite.Group()
-    pteras = pygame.sprite.Group()
-    clouds = pygame.sprite.Group()
-    last_obstacle = pygame.sprite.Group()
+        Cactus.containers = cacti
+        Ptera.containers = pteras
+        Cloud.containers = clouds
 
-    Cactus.containers = cacti
-    Ptera.containers = pteras
-    Cloud.containers = clouds
+        retbutton_image,retbutton_rect = load_image('replay_button.png',35,31,-1)
+        gameover_image,gameover_rect = load_image('game_over.png',190,11,-1)
 
-    retbutton_image,retbutton_rect = load_image('replay_button.png',35,31,-1)
-    gameover_image,gameover_rect = load_image('game_over.png',190,11,-1)
+        temp_images,temp_rect = load_sprite_sheet('numbers.png',12,1,11,int(11*6/5),-1)
+        HI_image = pygame.Surface((22,int(11*6/5)))
+        HI_rect = HI_image.get_rect()
+        HI_image.fill(background_col)
+        HI_image.blit(temp_images[10],temp_rect)
+        temp_rect.left += temp_rect.width
+        HI_image.blit(temp_images[11],temp_rect)
+        HI_rect.top = height*0.1
+        HI_rect.left = width*0.73
 
-    temp_images,temp_rect = load_sprite_sheet('numbers.png',12,1,11,int(11*6/5),-1)
-    HI_image = pygame.Surface((22,int(11*6/5)))
-    HI_rect = HI_image.get_rect()
-    HI_image.fill(background_col)
-    HI_image.blit(temp_images[10],temp_rect)
-    temp_rect.left += temp_rect.width
-    HI_image.blit(temp_images[11],temp_rect)
-    HI_rect.top = height*0.1
-    HI_rect.left = width*0.73
 
-    while not gameQuit:
-        while startMenu:
-            pass
-        while not gameOver:
-            if pygame.display.get_surface() == None:
-                print("Couldn't load display surface")
-                gameQuit = True
-                gameOver = True
-            else:
-
+        while playerDino.score <300:
                 #INPUTS
-                i_dino_bottom = height - dino_bottom
-                i_dino_height = dino_bottom - dino_top
-                i_dino_width = dino_right - dino_left
-                i_dist_cact = cact_left - dino_right
-                i_cact_width = cact_right - cact_left
-                i_cact_height = cact_bottom - cact_top
-                i_dist_bird = bird_left - dino_right
-                i_bird_width = bird_right - bird_left
-                i_bird_bottom = height - bird_bottom
-                i_fitness = cur_score + 0.0
-                i_gamespeed = gamespeed
+            i_dino_bottom = 1/(1+exp(height - dino_bottom))
+            i_dino_height = 1/(1+exp(dino_bottom - dino_top))
+            i_dino_width = 1/(1+exp(dino_right - dino_left))
+            i_dist_cact = 1/(1+exp(cact_left - dino_right))
+            i_cact_width = 1/(1+exp(cact_right - cact_left))
+            i_cact_height = 1/(1+exp(cact_bottom - cact_top))
+            i_dist_bird = 1/(1+exp(bird_left - dino_right))
+            i_bird_width = 1/(1+exp(bird_right - bird_left))
+            i_bird_bottom = 1/(1+exp(height - bird_bottom))
+            i_fitness = cur_score + 0.0
+            i_gamespeed = 1/(1+exp(gamespeed))
 
-                inputs = [i_dino_bottom, i_dino_height, i_dino_width, \
-                          i_dist_cact, i_cact_width, i_cact_height, \
-                          i_dist_bird, i_bird_width, i_bird_bottom, \
-                          i_gamespeed]
+            inputs = [i_dino_bottom, i_dino_height, i_dino_width, \
+                      i_dist_cact, i_cact_width, i_cact_height, \
+                      i_dist_bird, i_bird_width, i_bird_bottom, \
+                      i_gamespeed]
 
-                action = net.activate(inputs)
+            action = net.activate(inputs)
 
-                X = action[0]
-                Y = action[1]
+            X = action[0]
+            Y = action[1]
 
-                if X>=0.5:
-                    X=1
-                else:
-                    X=0
-                if Y>=0.5:
-                    Y=1
-                else:
-                    Y=0
+            if X>=0.5:
+                X=1
+            else:
+                X=0
+
+            if Y>=0.5:
+                Y=1
+            else:
+                Y=0
 
 
-                for event in pygame.event.get():
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameQuit = True
+                    gameOver = True
+            if X==1:
+                simulate_jump(playerDino)
+            if Y==1:
+                simulate_duck(playerDino)
+            if Y==0:
+                simulate_unduck(playerDino)
 
-                    if event.type == pygame.QUIT:
-                        gameQuit = True
-                        gameOver = True
-                #X=1
-                if X==1:
-                    simulate_jump(playerDino)
-                #Y=1
-                if Y==1:
-                    simulate_duck(playerDino)
-
-                if Y==0:
-                    simulate_unduck(playerDino)
-
-                #    if event.type == pygame.KEYDOWN:
-                #        if event.key == pygame.K_w:
-                #            if playerDino.rect.bottom == int(0.98*height):
-                #                playerDino.isJumping = True
-                #                if pygame.mixer.get_init() != None:
-                #                    jump_sound.play()
-                #                playerDino.movement[1] = -1*playerDino.jumpSpeed
-                #
-                #        if event.key == pygame.K_s:
-                #            if not (playerDino.isJumping and playerDino.isDead):
-                #                playerDino.isDucking = True
-                #
-                #    if event.type == pygame.KEYUP:
-                #        if event.key == pygame.K_s:
-                #            playerDino.isDucking = False
             for c in cacti:
                 c.movement[0] = -1*gamespeed
                 if pygame.sprite.collide_mask(playerDino,c):
@@ -556,17 +476,6 @@ def gameplay(net, fitnesses):
                 last_obstacle.empty()
                 last_obstacle.add(Cactus(gamespeed,40,40))
 
-
-            #if len(cacti) < 2:
-            #    if len(cacti) == 0:
-            #        last_obstacle.empty()
-            #        last_obstacle.add(Cactus(gamespeed,40,40))
-            #    else:
-            #        for l in last_obstacle:
-            #            if l.rect.right < width*0.7 and random.randrange(0,50) == 10:
-            #                last_obstacle.empty()
-            #                last_obstacle.add(Cactus(gamespeed, 40, 40))
-
             if(len(pteras) == 0):
                 bird_left = 0
                 bird_right = 0
@@ -576,7 +485,6 @@ def gameplay(net, fitnesses):
                     if l.rect.right < width*0.8:
                         last_obstacle.empty()
                         last_obstacle.add(Ptera(gamespeed, 46, 40))
-
 
             if len(clouds) < 5 and random.randrange(0,300) == 10:
                 Cloud(width,random.randrange(height/5,height/2))
@@ -614,84 +522,21 @@ def gameplay(net, fitnesses):
             if counter%700 == 699:
                 new_ground.speed -= 1
                 gamespeed += 1
+
             game_speed = gamespeed
-
             counter = (counter + 1)
+            if gameQuit:
+                break
+            if gameOver:
+                break
+            fitness = cur_score
+        fitnesses.append(fitness)
+    return max(fitnesses)
 
-            '''
-            #print 'dino_left', dino_left #const
-            #print 'dino_right', dino_right #const
-            #print 'dino_top', dino_top #mapped
-            #print 'dino_bottom', dino_bottom #mapped
-            ####################################################################
-            #print 'bird_left', bird_left #mapped
-            #print 'bird_right', bird_right #mapped
-            #print 'bird_top', bird_top #const
-            #print 'bird_bottom', bird_bottom #const
-            #####################################################################
-            #print 'cact_left', cact_left #mapped
-            #print 'cact_right', cact_right #mapped
-            #print 'cact_top', cact_top #const
-            #print 'cact_bottom', cact_bottom #const
-            ####################################################################
-            #print 'game_speed', game_speed #mapped
-            #print 'cur_score', cur_score #mapped
-            #print '\n'
-            ####################################################################
-            #INPUTS
-            '''
-
-
-
-        if gameQuit:
-            break
-
-        while gameOver:
-            fitnesses.append(i_fitness)
-
-            if pygame.display.get_surface() == None:
-                print("Couldn't load display surface")
-                gameQuit = True
-                gameOver = False
-            else:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        gameQuit = True
-                        gameOver = False
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            gameQuit = True
-                            gameOver = False
-
-                        #if event.key == pygame.K_RETURN or event.key == pygame.K_w:
-                        #    gameOver = False
-                        #    gameplay()
-
-            highsc.update(high_score)
-            if pygame.display.get_surface() != None:
-                disp_gameOver_msg(retbutton_image,gameover_image)
-                if high_score != 0:
-                    highsc.draw()
-                    screen.blit(HI_image,HI_rect)
-                pygame.display.update()
-            clock.tick(FPS)
-            return min(fitnesses)
-
-
-    #pygame.quit()
-    #quit()
-
-def eval_genome(genome, config):
-    net = neat.nn.FeedForwardNetwork.create(genome, config)
-
-    fitnesses = []
-    isGameQuit = introscreen()
-
-    for runs in range(runs_per_net):
-        fitness = 0.0
-        if not isGameQuit:
-            return gameplay(net, fitnesses)
-
+#EXISTENCE IS A DOUBT
+#def eval_genomes(genomes, config):
+#    for genome_id, genome in genomes:
+#        genome.fitness = eval_genome(genome, config)
 
 def run():
     local_dir = os.path.dirname(__file__)
@@ -702,22 +547,16 @@ def run():
 
     pop = neat.Population(config)
     stats = neat.StatisticsReporter()
-
     pop.add_reporter(stats)
     pop.add_reporter(neat.StdOutReporter(True))
-
-    pe = neat.ParallelEvaluator(4, eval_genome)
+    pe = neat.ParallelEvaluator(1, gameplay)
     winner  = pop.run(pe.evaluate)
-
     with open('winner-feedforward', 'wb') as f:
         pickle.dump(winner, f)
-
     print winner
-
     pygame.quit()
     quit()
 
 def main():
     run()
-
 main()
